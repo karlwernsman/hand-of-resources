8df371ea0396307ea3b9cc13d36b805aea08cac5
karl wernsman <wernsmank@gmail.com>
2022-11-09 15:49:01 -0800

    setup sql for resource 1

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    age INT NOT NULL,
+    breed VARCHAR NOT NULL
+);
+
+INSERT INTO 
+    dogs (name, age, breed)
+VALUES
+    ('Lottie', 4, 'Pit Mix'),
+    ('Rudy', 10, 'Pit Mix'),
+    ('Laikia', 2, 'Min Pin'),
+    ('Lucy', 4, 'German Shorthair'),
+    ('Rascal', 12, 'Super Mutt');
