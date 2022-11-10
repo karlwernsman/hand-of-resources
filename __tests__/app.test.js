5414c79369e9223072c10053880f93b6f7c3ff98
karl wernsman <wernsmank@gmail.com>
2022-11-09 16:13:59 -0800

    index test for resource 1 passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('dog routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /dogs should return a list of dogs', async () => {
+    const resp = await request(app).get('/dogs');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(5);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      age: expect.any(Number),
+      breed: expect.any(String),
+    });
   });
   afterAll(() => {
     pool.end();
